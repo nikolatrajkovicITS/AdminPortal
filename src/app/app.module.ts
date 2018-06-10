b53c3ddc9e38de05262afd6549b7bf4f846aa780
NikolaTrajkovic <nikolatrajkovic.contact@gmail.com>
2018-06-10 13:21:35 +0200

    Add client details component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { FlashMessagesModule } from 'angular2-flash-messages';
 // AngularFire Imports
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabase } from 'angularfire2/database';
@@ -26,7 +27,8 @@ const appRoutes: Routes = [
   {path:'', component:DashboardComponent},
   {path:'register', component:RegisterComponent},
   {path:'login', component:LoginComponent},
-  {path:'add-client', component:AddClientComponent}
+  {path:'add-client', component:AddClientComponent},
+  {path:'client/:id', component:ClientDetailsComponent}
 ];
 
 export const firebaseConfig = {
@@ -56,7 +58,8 @@ export const firebaseConfig = {
     BrowserModule,
     FormsModule,
     RouterModule.forRoot(appRoutes),
-    AngularFireModule.initializeApp(firebaseConfig)
+    AngularFireModule.initializeApp(firebaseConfig),
+    FlashMessagesModule,
   ],
   providers: [
     AngularFireAuth,
